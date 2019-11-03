4901a6875ca1d7bda49ce3f2903c32930a8d5659
RishiKL <52776758+ris-public@users.noreply.github.com>
2019-11-03 15:00:29 +0530

    https://www.internic.net/domain/root.zone

--- cron.sh
+++ cron.sh
@@ -1,4 +1,6 @@
-curl -Os https://www.internic.net/domain/root.zone
+cd /var/www/html/unpr/root-zone-watch
+PATH=/home/unpr/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/unpr/.dotnet/tools:/home/unpr/.dotnet/tools
+curl -s https://www.internic.net/domain/root.zone|egrep -v "(RRSIG|SOA)">root.zone
 git add root.zone
 git commit -m "https://www.internic.net/domain/root.zone"
 git push
